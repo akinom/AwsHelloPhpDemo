1f73ffb7a869bf4b6703e00b55babed9e3cece89
Monika Mevenkamp <monikam@princeton.edu>
2017-06-01 13:47:18 -0400

    AWS: get PHP code from git

--- index.php
+++ index.php
@@ -1,46 +1,17 @@
 <html>
-<head>
-    <title>AWS CloudFormation PHP cwdeployed from Git</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-</head>
-<body>
-    <h1>Welcome to the current Version </h1>
-    <p/>
-    <?php
-          // Print out the current data and time
-          print "The Current Date and Time is: <br/>";
-          print date("g:i A l, F j Y.");
-    ?>
-    <p/>
-
-    <?php
-          // Setup a handle for CURL
-          $curl_handle=curl_init();
-          curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
-          curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
-          // Get the hostname of the intance from the instance metadata
-          curl_setopt($curl_handle,CURLOPT_URL,'http://169.254.169.254/latest/meta-data/public-hostname');
-          $hostname = curl_exec($curl_handle);
-          if (empty($hostname))
-          {
-            print "Sorry, for some reason, we got no hostname back <br />";
-          } else {
-            print "Server = " . $hostname . "<br/>";
-          }
-          // Get the instance-id of the intance from the instance metadata
-          curl_setopt($curl_handle,CURLOPT_URL,'http://169.254.169.254/latest/meta-data/instance-id');
-          $instanceid = curl_exec($curl_handle);
-          if (empty($instanceid))
-          {
-             print "Sorry, for some reason, we got no instance id back <br/>";
-          } else {
-             print "EC2 instance-id = " . $instanceid . "<br/>";
-          }
-    ?>
-    <h2>PHP Information</h2>
-    <p/>
-    <?php
-          phpinfo();
-    ?>
-</body>
+    <head>
+        <title>Deployed with AWS</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    </head>
+    <body>
+        <h1>Here it Is - Fresh from AWS </h1>
+        <div>
+            <?php
+            print "Hello there";
+            ?>
+        </div>
+        <div>
+        <a href="./LAMP_php.json"> deployed with AWS template </a>
+        </div>
+    </body>
 </html>
